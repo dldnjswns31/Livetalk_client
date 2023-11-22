25f368a90153dec69b8e3ad31ca37d7573a4e53f
dldnjswns31 <dldnjswns32@gmail.com>
2023-11-22 13:02:10 +0900

    fix: auth routing bug fix - auth routing logic으로 페이지 이동 시 깜빡임 현상 제거

--- src/components/HOC/withAuth.tsx
+++ src/components/HOC/withAuth.tsx
@@ -1,24 +1,31 @@
-import { ComponentType, useEffect } from "react";
+import { ComponentType, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getToken } from "../../utils/token";
 
 export const withAuth =
   (Component: ComponentType, auth: boolean) =>
   <P extends object>(props: P) => {
+    const [isAuthentication, setIsAuthentication] = useState<boolean | null>(
+      null
+    );
+
     const navigate = useNavigate();
+
     useEffect(() => {
       const token = getToken();
-      if (auth) {
-        if (!token) {
-          navigate("/login");
-        }
-      } else {
-        if (token) {
-          navigate("/");
-        }
+      if (auth && !token) {
+        navigate("/login");
       }
-    }, []);
-    return <Component {...props} />;
+
+      // 인증 필요 없는 route && 토큰 존재 시 채팅방으로 자동 이동
+      if (!auth && token) {
+        navigate("/");
+      }
+
+      setIsAuthentication(token !== null);
+    }, [navigate]);
+
+    return isAuthentication === null ? null : <Component {...props} />;
   };
 
 export default withAuth;
